c6b8446a872add82d65dd77f20dc8db18fc184d2
EC2 Default User <ec2-user@ip-172-31-89-237.ec2.internal>
2018-11-13 00:08:26 +0000

    hello

--- common/models/contact.js
+++ common/models/contact.js
@@ -1,5 +1,29 @@
 'use strict';
+var isEmail = require('isemail');
 
 module.exports = function(Contact) {
+
+    Contact.getVerifyOptions = function() {
+    	const defaultOptions = {
+      	    type: 'email',
+            from: 'noreply@example.com',
+        };
+       return Object.assign({}, this.settings.verifyOptions || defaultOptions)
+     };
+
+    function assertVerifyOptions(verifyOptions) {
+        assert(verifyOptions.type, 'You must supply a verification type (verifyOptions.type)');
+        assert(verifyOptions.type === 'email', 'Unsupported verification type');
+        assert(verifyOptions.to, 'Must include verifyOptions.to when calling user.verify() ' + 
+              'or the user must have an email property');
+        assert(verifyOptions.from, 'Must include verifyOptions.from when calling user.verify()');
+        assert(typeof verifyOptions.templateFn === 'function',
+              'templateFn must be a function');
+        assert(typeof verifyOptions.generateVerificationToken === 'function',
+              'generateVerificationToken must be a function');
+        assert(verifyOptions.mailer, 'A mailer function must be provided');
+        assert(typeof verifyOptions.mailer.send === 'function', 'mailer.send must be a function ');
+  }
+
 
 };
