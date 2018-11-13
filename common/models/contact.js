b25e62a8a78c3c29ccd513ac5c3e44fc59b67f45
EC2 Default User <ec2-user@ip-172-31-89-237.ec2.internal>
2018-11-13 00:20:41 +0000

    add email verifiation for contact email

--- common/models/contact.js
+++ common/models/contact.js
@@ -1,5 +1,6 @@
 'use strict';
 var isEmail = require('isemail');
+var re = /^(([^<>()[\]\\.,;:\s@\"]-(\.[^<>()[\]\\.,;:\s@\"]-)*)|(\".-\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]-\.)-[a-zA-Z]{2,}))$/;
 
 module.exports = function(Contact) {
 
@@ -10,7 +11,7 @@ module.exports = function(Contact) {
         };
        return Object.assign({}, this.settings.verifyOptions || defaultOptions)
      };
-
+    Contact.validatesFormatOf('email', {with: re, message: 'Must provide a valid email'});
     function assertVerifyOptions(verifyOptions) {
         assert(verifyOptions.type, 'You must supply a verification type (verifyOptions.type)');
         assert(verifyOptions.type === 'email', 'Unsupported verification type');
