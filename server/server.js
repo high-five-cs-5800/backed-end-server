ecd8638001d39f9d674333d333a23b4d50f4e8d8
EC2 Default User <ec2-user@ip-172-31-89-237.ec2.internal>
2018-11-17 22:43:02 +0000

    cors issue

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 //#
-//var cors = require('cors')
+var cors = require('cors')
 //#
-//app.use(cors())
+app.use(cors())
 app.use(function(req, res, next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
