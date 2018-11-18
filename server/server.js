f5871c2108b91a83ab75cf49f5625f909160c645
EC2 Default User <ec2-user@ip-172-31-89-237.ec2.internal>
2018-11-18 01:50:07 +0000

    cors

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ app.use(function (req, res, next) {
 });
 
 
-app.use(cors())
+//app.use(cors())
 //app.use(function(req, res, next){
 //  res.header("Access-Control-Allow-Origin", "*");
 //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
