7a70c7b1f3a85a4dce73b84dc9080fde0d0b26cd
EC2 Default User <ec2-user@ip-172-31-89-237.ec2.internal>
2018-11-18 01:33:13 +0000

    cors

--- server/server.js
+++ server/server.js
@@ -21,12 +21,12 @@ var corsOptions = {
 
 
 //here is the magic
-app.use(cors(corsOptions), function(req, res, next){  
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-    
-});
+//app.use(cors(corsOptions), function(req, res, next){  
+//  res.header("Access-Control-Allow-Origin", "*");
+//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//  next();
+//    
+//});
 
 app.use(function (req, res, next) {
 
@@ -48,7 +48,7 @@ app.use(function (req, res, next) {
 });
 
 
-//app.use(cors())
+app.use(cors())
 //app.use(function(req, res, next){
 //  res.header("Access-Control-Allow-Origin", "*");
 //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
