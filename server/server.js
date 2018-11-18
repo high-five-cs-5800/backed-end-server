becbe3634e6f3e92fa8e78b1da6120ebcf3d4a2c
EC2 Default User <ec2-user@ip-172-31-89-237.ec2.internal>
2018-11-18 03:08:27 +0000

    cors

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ var app = module.exports = loopback();
 //#
 var cors = require('cors')
 //#
-var wWhitelist = [
+var whitelist = [
   'http://testdev-us-east-1.s3-website-us-east-1.amazonaws.com/',      //this is my front-end url for development
    'http://testdev-us-east-1.s3-website-us-east-1.amazonaws.com:80/'
 ];
