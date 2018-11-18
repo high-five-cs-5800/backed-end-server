ca13b2bfce5c09a238aa0fc831bf1ec395671278
EC2 Default User <ec2-user@ip-172-31-89-237.ec2.internal>
2018-11-18 03:19:08 +0000

    cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ var cors = require('cors')
 //#
 var whitelist = [
   'http://testdev-us-east-1.s3-website-us-east-1.amazonaws.com/*',      //this is my front-end url for development
-  'http://testdev-us-east-1.s3-website-us-east-1.amazonaws.com:80/*'
+  'http://testdev-us-east-1.s3-website-us-east-1.amazonaws.com:80/*',
+  '*'
 ];
 
 var corsOptions = {
