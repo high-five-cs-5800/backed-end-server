828b6df9495983a6a8ee1a328b8003797da1aa60
EC2 Default User <ec2-user@ip-172-31-89-237.ec2.internal>
2018-11-18 00:30:14 +0000

    cors

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ var corsOptions = {
 
 
 //here is the magic
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 
 app.use(cors())
 app.use(function(req, res, next){
@@ -36,11 +36,11 @@ app.start = function() {
    //what is going on 
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
-    //app.get('/api/UserAccounts', function(req, res, next){
-    //    //res.json({msg: 'This is CORS-ENABLE for all origins!'})
-    //      res.header("Access-Control-Allow-Origin", "http://localhost:8081");
-    //      res.header("Access-Control-Allow-Headers");
-    //})
+    app.get('/api/UserAccounts', function(req, res, next){
+        res.json({msg: 'This is CORS-ENABLE for all origins!' + baseUrl})
+          res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+          res.header("Access-Control-Allow-Headers");
+    })
     
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
