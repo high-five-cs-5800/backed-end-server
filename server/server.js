06903cdec94c737c66db9f24b261a51a6791174c
EC2 Default User <ec2-user@ip-172-31-89-237.ec2.internal>
2018-11-18 00:46:56 +0000

    cors config

--- server/server.js
+++ server/server.js
@@ -28,6 +28,15 @@ app.use(cors(corsOptions), function(req, res, next){
     
 });
 
+app.all("/api/*", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization,   Content-Type, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    return next();
+});
+
+
+
 //app.use(cors())
 //app.use(function(req, res, next){
 //  res.header("Access-Control-Allow-Origin", "*");
